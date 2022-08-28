a93ca73042f2a0f06e13c99de9fe6f16eee91354
firsovvs <7513325Qq>
2022-08-28 23:31:15 +0300

    Initial commit

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import * as cookieParser from 'cookie-parser';
+import { config } from 'dotenv'
+import { AppModule } from './app.module'
+config()
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+	app.use(cookieParser());
+	app.enableCors()
+
+	const config = await app.get(ConfigService)
+	const port = Number(config.get('main.port'))
+	const host = String(config.get('main.host'))
+
+	await app.listen(port, () => {
+		console.log(`Сервер доступен - http://${host}:${port}/graphql`)
+	})
+}
+
+bootstrap()
