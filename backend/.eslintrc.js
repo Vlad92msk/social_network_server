73222437fde6d7901b0da8a935b389e291d0837d
firsovvs <7513325Qq>
2022-08-29 01:07:19 +0300

    продолжаю тестить настройку проекта

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   root: true,
   env: {
     node: true,
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,37 +1,44 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { GraphQLModule } from '@nestjs/graphql'
-import { UsersModule } from './users/users.module'
+import {Module} from '@nestjs/common'
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {ConfigModule, ConfigService} from '@nestjs/config'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {GraphQLModule} from '@nestjs/graphql'
+import {ApolloServerPluginLandingPageLocalDefault} from 'apollo-server-core';
 import mainConfig from './config/main.config'
+import {AuthModule} from "./lib/auth/auth.module";
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({ load: [mainConfig], isGlobal: true, envFilePath: '../.env' }),
-		GraphQLModule.forRoot({
-			autoSchemaFile: 'schema.gql',
-			sortSchema: true,
-			playground: true,
-		}),
-		TypeOrmModule.forRootAsync({
-			imports: [ ConfigModule ],
-			inject: [ ConfigService ],
-			useFactory: async (config: ConfigService) => ({
-				type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION'),
-				host: config.get<string>('TYPEORM_HOST'),
-				username: config.get<string>('TYPEORM_USERNAME'),
-				password: config.get<string>('TYPEORM_PASSWORD'),
-				database: config.get<string>('TYPEORM_DATABASE'),
-				port: config.get<number>('TYPEORM_PORT'),
-				entities: [ __dirname + 'dist/**/*.entity{.ts,.js}' ],
-				synchronize: true,
-				autoLoadEntities: true,
-				logging: true,
-			}),
-		}),
-		UsersModule,
-	],
-	providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [mainConfig],
+      isGlobal: true,
+      envFilePath: '../.env'
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: true,
+      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION'),
+        host: config.get<string>('TYPEORM_HOST'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/lib/**/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {
 }
